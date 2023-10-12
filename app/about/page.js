af0fe2e7697fad32a1ae9e0d8c20fb2d3772431e
Vitalii MV <agronom9913@gmail.com>
2023-10-12 21:57:17 +0300

    2023.10.18 #4

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function About() {
+  return (
+    <div>
+    <h1 className="">Сторінка про нас</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ullam provident, perspiciatis reiciendis vel dolorum quidem ipsam animi maxime facilis explicabo tempora numquam laboriosam labore magnam saepe, dolorem veniam? Quae?
+      </p>
+    </div>
+  )
+}
+
--- app/page.js
+++ app/page.js
@@ -1,6 +1,24 @@
+import Link from "next/link";
 
-export default function Home() {
+async function fetchData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const result = await res.json();
+  return result
+}
+
+export default async function Home() {
+  const posts = await fetchData();
+
   return (
-   <h1>Головна сторінка</h1>
-  )
+    <div>
+      <h1>Головна сторінка </h1>
+    {posts.map(el =>(
+      <div key={el.id} className="post">
+        <h2>{el.title}</h2>
+        <p>{el.body}</p>
+        <Link href={'/post/'+ el.id}>Детально</Link>
+      </div>
+    ))}
+    </div>
+  );
 }
